254c233532d2991b70d292e14619ac039a4c75a0
suriya-wsl <suriyaprakhash@gmail.com>
2024-04-17 07:55:05 -0400

    updated captcha for testing

---
+++ assets/js/contact-us.js
@@ -0,0 +1,50 @@
+//contact-us.js
+$( document ).ready(function() {
+
+    const form = document.getElementById('my-form');
+    const result = document.getElementById('form-result');
+    
+    //on submit, POST to web3Forms and get response
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      const formData = new FormData(form);
+      const object = Object.fromEntries(formData);
+      const json = JSON.stringify(object);
+      result.innerHTML = "Please wait..."
+      //fetch result
+      fetch('https://api.web3forms.com/submit', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+              },
+              body: json
+          })
+          .then(async (response) => {//show response
+              let json = await response.json();
+              console.log(json);
+              if (response.status == 200) {
+                  result.innerHTML = json.message;
+                  result.classList.add("notice--success");
+              } else {
+                  console.log(response);
+                  result.innerHTML = json.message;
+                  result.classList.add("notice--warning");
+    
+              }
+          })
+          .catch(error => {//show error
+              console.log(error);
+              result.innerHTML = "Something went wrong!";
+              result.classList.add("notice--danger");
+    
+          })
+          .then(function() {//reset form
+              form.reset();
+              //make response dissapear after 3 seconds if you wish
+              setTimeout(() => {
+                result.style.display = "none";
+              }, 3000);
+          });
+    });
+  });
